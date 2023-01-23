0cdbf1da0465b261d1ef47ddd688a4e746ac8210
pgarrett <pgarrett@scripps.edu>
2023-01-23 14:58:22 -0800

    v0.1.0

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
+from pathlib import Path
+
 from setuptools import setup
+
+with open("README", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+req = 'requirements.txt'
+if Path(req).is_file():
+    with open(req) as f:
+        requirements = f.read().splitlines()
+else:
+    requirements = []
 
 setup(
     name='exclusionms',
-    version='0.0.11',
+    version='0.1.0',
     packages=['exclusionms'],
     url='',
     license='',
-    author='pgarrett',
+    author='Patrick Garrett',
     author_email='pgarrett@scripps.edu',
-    description='',
-    install_requires=[
-        'intervaltree==3.1.0',
-        'requests==2.28.1',
-        ],
+    description=long_description,
+    install_requires=requirements,
+    python_requires='>=3.6'
 )
